dd4741be3b97d7d6046e2922209705f63c3bfea3
Justin Watts <tk8817@users.noreply.github.com>
2018-01-29 21:01:14 -0500

    Update browser.rb

--- lib/ferris/concepts/browser.rb
+++ lib/ferris/concepts/browser.rb
@@ -15,7 +15,7 @@ module Ferris
                      profile:           'user-data-dir=****',
                      size:              '--window-size=****',
                      user_agent:        '--user-agent=****',
-                     ignore_ssl_errors: '--ignore-certificate-errors --ignore-certificate-errors-spki-list',
+                     ignore_ssl_errors: '--ignore-certificate-errors',
                      headless_insecure: '--acceptInsecureCerts=****',
                      allow_insecure:    '--allow-running-insecure-content' }.freeze
 
