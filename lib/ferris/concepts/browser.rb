d00810c4d225e030cadfa6febe82a140c55ccb94
Justin Watts <tk8817@users.noreply.github.com>
2018-01-29 20:32:19 -0500

    Update browser.rb

--- lib/ferris/concepts/browser.rb
+++ lib/ferris/concepts/browser.rb
@@ -16,7 +16,7 @@ module Ferris
                      size:              '--window-size=****',
                      user_agent:        '--user-agent=****',
                      ignore_ssl_errors: '--ignore-certificate-errors',
-                     headless_insecure: '--accept_insecure_certs',
+                     headless_insecure: '--accept-insecure-certs=****',
                      allow_insecure:    '--allow-running-insecure-content' }.freeze
 
       PREF_MAP   = { geolocation: :managed_default_content_settings,
